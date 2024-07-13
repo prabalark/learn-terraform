f500b123237e4d442b14e25cc356148912b8126a
prabala <radhakrishnaprabala8@gmail.com>
2024-07-13 15:59:13 +0530

    main.tf ver1

--- ansible-terraform/main.tf
+++ ansible-terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "para1" {
-  name  = "test5"    # keep folder name   # system manger in aws
-  #type  = "String"
+  name  = var.practice1[count.index].name   # keep folder name   # system manger in aws
+  value = var.practice1[count.index].value    # any text
+  #type = "String"
   type  = "SecureString"   # [txt="String" ,"StringList" ,encrpt="SecureString"]
-  value = "hello"    # any text
   key_id = "8dae0259-1d22-47ef-a533-2cf7a81cb5c5" # kms -to encrpt data
 }
