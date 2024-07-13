dd081d1c5944f036d055107df366e89b7a4ada6e
prabala <radhakrishnaprabala8@gmail.com>
2024-07-13 15:35:32 +0530

    main.tf vr1

--- ansible-terraform/main.tf
+++ ansible-terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "para1" {
-  name  = "test2"    # keep folder name   # system manger in aws
+  name  = "test3"    # keep folder name   # system manger in aws
   type  = "String"   # ["String" "StringList" "SecureString"]
   value = "hello"         # any text
 }
