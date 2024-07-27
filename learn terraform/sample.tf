7809f7d3cda984e8bd651e5e5f4b675fe4790149
prabala <radhakrishnaprabala8@gmail.com>
2024-07-27 15:37:46 +0530

    sample.tf vr7

--- learn terraform/sample.tf
+++ learn terraform/sample.tf
@@ -1,9 +1,9 @@
 ## for dynamic pick up of ami
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "Centos-8-DevOps-Practice"
+#  owners      = ["973714476881"]
+#}
 
 ## instance creation
 resource "aws_instance" "web" {
@@ -18,11 +18,11 @@ output "web" {
   value = aws_instance.web.public_ip
 }
 
-# create record
-resource "aws_route53_record" "reccord" {
-  zone_id = "Z03484922ZJTTIW47BAFL"
-  name    = "frnd.devops72bat.online"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.web.public_ip]
-}
+## create record
+#resource "aws_route53_record" "reccord" {
+#  zone_id = "Z03484922ZJTTIW47BAFL"
+#  name    = "frnd.devops72bat.online"
+#  type    = "A"
+#  ttl     = 1
+#  records = [aws_instance.web.public_ip]
+#}
